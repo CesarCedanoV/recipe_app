8abe5f810e9a3301b2ea59790bedc64154647e3e
CesarCedanoV <CesarCedanoV@gmail.com>
2018-11-19 21:50:59 -0400

    Added the search view and also integrate some functions that will return a List of Recipes

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,27 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 const state = {};
 
 const controlSearch = async () => {
-  const query = 'pizza';
+  const query = searchView.getInputValue();
 
   if (query) {
     state.search = new Search(query);
 
-    
+    searchView.clearInputValue();
+    searchView.clearResultsList();
+
     await state.search.getResults();
 
-    console.log(state.search.results)
+
+    searchView.renderResults(state.search.results);
   }
 
 }
 
-document.querySelector('.search')
-  .addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
   });
