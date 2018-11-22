30ad2aca6aa619f82cad8c09bcb771a11e8ca25b
Cesar A. Cedano V <ccedanovelez@crestviewc.com>
2018-11-22 17:57:48 -0400

    Created the controlRecipe to get the Recipe and implement the hash event to get the Recipe ID

--- src/js/index.js
+++ src/js/index.js
@@ -30,22 +30,38 @@ const controlSearch = async () => {
 
 }
 
-
-const recipe = new Recipe(47746);
-recipe.getRecipe(46956);
-console.log(recipe)
-
-
 elements.searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    controlSearch();
-  });
+  e.preventDefault();
+  controlSearch();
+});
 
-  elements.searchResultsPages.addEventListener('click', e => {
-    const btn = e.target.closest('.btn-inline');
-    if (btn) {
-      const goToPage = parseInt(btn.dataset.goto);
-      searchView.clearResultsList();
-      searchView.renderResults(state.search.results, 10,goToPage);
+elements.searchResultsPages.addEventListener('click', e => {
+  const btn = e.target.closest('.btn-inline');
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto);
+    searchView.clearResultsList();
+    searchView.renderResults(state.search.results, 10,goToPage);
+  }
+});
+
+const controlRecipe = async () => {
+  const id = window.location.hash.replace('#','');
+  console.log(id);
+
+  if (id){
+    state.recipe = new Recipe(id);
+    try{
+      await state.recipe.getRecipe();
+  
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+  
+      console.log(state.recipe);
+    }catch (err){
+      console.log(err);
+      alert('Error processing Recipe');
     }
-  });
+  } 
+};
+
+['hashchange','load'].forEach(event => window.addEventListener(event, controlRecipe));
