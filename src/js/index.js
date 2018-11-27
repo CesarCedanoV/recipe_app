1bd718d0f5e2ab4fbaa44d3c2f78c0a6215fdd14
Cesar A. Cedano V <ccedanovelez@crestviewc.com>
2018-11-27 17:55:48 -0400

    Started the List View

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 const state = {};
@@ -76,6 +78,16 @@ const controlRecipe = async () => {
 
 ['hashchange','load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+const controlList = () => {
+  if (!state.list) state.list = new List();
+
+  state.recipe.ingredients.forEach(el => {
+    console.log(el);
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+}
+
 elements.recipe.addEventListener('click', e => {
   if (e.target.matches('.btn-decrease, .btn-decrease *') ) {
     if (state.recipe.servings > 1){
@@ -85,6 +97,12 @@ elements.recipe.addEventListener('click', e => {
   } else if (e.target.matches('.btn-increase, .btn-increase *') ) {
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
+    console.log('Pase por aqui');
+    controlList();
   }
-  console.log(state.recipe);
-})
+});
+
+
+
+window.l = new List();
